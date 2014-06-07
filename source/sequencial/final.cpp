9683a9600b00009d1cea77ed8dd1695f3fc32128
Matheus <matheus.mtxr@gmail.com>
2014-06-07 19:47:26 -0300

    Write PPM function 100% working

--- source/sequencial/final.cpp
+++ source/sequencial/final.cpp
@@ -9,11 +9,15 @@
 
 using namespace std;
 
-//imprime as mensagens de saida se DEBUG_MODE estiver definido
+/*
+imprime as mensagens de saida se DEBUG_MODE estiver definido
+a macro __func__ imprime o nome da função que chamou o log
+*/
+
 #ifdef DEBUG_MODE
-#define DEBUG(str) do { cout << "DEBUG: " << str << endl; } while(false)
+#define LOG(str) do { cout << "[" << __func__ <<"]=> " << str << endl; } while(false)
 #else
-#define DEBUG(str) do { } while(false)
+#define LOG(str) do { } while(false)
 #endif
 
 typedef struct Image_Matrix{
@@ -37,8 +41,26 @@ Image_Matrix *newPPM(int w, int h, int max){
     tmp->g = (int*) malloc(sizeof(int)*w*h);
     tmp->b = (int*) malloc(sizeof(int)*w*h);
 
-    DEBUG("Criando imagem de tamanho " << w <<'x'<<h);
+    LOG("Criando imagem em branco de tamanho " << w <<'x'<<h);
     return tmp;
+}
+
+Image_Matrix *smoothPPM(Image_Matrix *in){
+    LOG("Aplicando filtro Smooth");
+    Image_Matrix *out;
+    out=newPPM(in->width, in->height, in->max_color);
+
+    int w = out->width;
+    int h = out->height;
+    int pos;
+    /*faz a aplicação do filtro da imagem */
+    for(pos=0;pos<w*h;pos++){
+        out->r[pos]=in->r[pos];
+        out->g[pos]=in->g[pos];
+        out->b[pos]=in->b[pos];
+    }
+
+    return out;
 }
 
 //le uma imagem PPM
@@ -51,7 +73,6 @@ Image_Matrix *readPPM(string image){
     ppmfile = fopen(image.c_str(),"r");
     //le a primeira linha e descarta
     fgets(buffer,BUFFER_SIZE, ppmfile);
-    DEBUG("firstline: " << buffer);
 
     /* le uma linha e verifica se não é comentario */
     /* referencia http://stackoverflow.com/questions/2693631/read-ppm-file-and-store-it-in-an-array-coded-with-c */
@@ -66,7 +87,6 @@ Image_Matrix *readPPM(string image){
 
     /* pega altura e largura da imagem*/
     fscanf(ppmfile,"%d %d",&tmp->width, &tmp->height);
-    DEBUG("size: " << tmp->width << "x" << tmp->height);
 
     int w = tmp->width;
     int h = tmp->height;
@@ -77,21 +97,53 @@ Image_Matrix *readPPM(string image){
 
     /*pega o max color da imagem*/
     fscanf(ppmfile,"%d",&tmp->max_color);
-    DEBUG("max color: " << tmp->max_color);
 
-    /* pega o valor max color */
-    for(int lim=0,pos=0;lim<w*h;lim=lim+3,pos++){
-        int r,g,b;
+    /* le as trinas rgb da imagem*/
+    int pos;
+    for(pos=0;pos<w*h;pos++){
         fscanf(ppmfile,"%d %d %d",&tmp->r[pos], &tmp->g[pos], &tmp->b[pos]);
     }
 
-    DEBUG("Imagem '" << image << "' de tamanho " << tmp->width <<'x'<< tmp->height << " lida");
+    LOG("Imagem '" << image << "' de tamanho " << tmp->width <<'x'<< tmp->height << " lida");
 
     fclose(ppmfile);
     return tmp;
 }
-void deletePPM(Image_Matrix *image){
-    DEBUG("Limpando matrizes da imagem e deletando ponteiro");
+
+void writePPM(string filename, Image_Matrix *img){
+    FILE *ppmfile;
+
+    ppmfile = fopen(filename.c_str(), "w");
+    /* escreve o tipo de imagem */
+    fprintf(ppmfile, "P3\n");
+
+    /* escreve o nome da imagem como comentario */
+    fprintf(ppmfile, "# %s\n", filename.c_str());
+
+    /* escreve as dimensões da imagem no arquivo */
+    fprintf(ppmfile, "%d %d\n", img->width, img->height);
+    /* escreve o max color na imagem */
+    fprintf(ppmfile, "%d\n", img->max_color);
+
+    int w = img->width;
+    int h = img->height;
+    int pos;
+    /* escreve as trinas rgb da imagem */
+    for(pos=0;pos<w*h;pos++){
+        fprintf(ppmfile,"%d %d %d",img->r[pos], img->g[pos], img->b[pos]);
+        if(pos%img->width!=0)
+            fprintf(ppmfile, " ");
+        else
+            fprintf(ppmfile, "\n");
+
+    }
+    fprintf(ppmfile,"\n");
+
+    LOG("Imagem '" << filename << "' salva com sucesso");
+}
+
+void deletePPMmatrix(Image_Matrix *image){
+    LOG("Limpando matrizes da imagem e deletando ponteiro");
     free(image->r);
     free(image->g);
     free(image->b);
@@ -104,14 +156,17 @@ int main(int argc, char *argv[])
     Image_Matrix *in;
     Image_Matrix *out;
 
-    DEBUG("Exibindo mensagens de saida.");
+    LOG("Exibindo mensagens de saida no formato [nome_funcao]: mensagem");
 
     in = readPPM(filename);
-    out = newPPM(in->width,in->height,in->max_color);
+
+    out = smoothPPM(in);
+
+    writePPM("out.ppm",out);
 
     //faz um free nas matrizes das imagens e deleta o ponteiros
-    deletePPM(in);
-    deletePPM(out);
+    deletePPMmatrix(in);
+    deletePPMmatrix(out);
 
     return 0;
 }
